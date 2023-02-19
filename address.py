9cefcbc429aa063d7a46b794f88eec50a6d1af05
Isaac Arredondo Padrón <isaacarredondopadron@gmail.com>
2023-02-19 15:29:52 -0600

    delete duplicated files

--- address.py
+++
@@ -1,7 +0,0 @@
-class Address:
-    def __init__(self, country, state, city, street, postal):
-        self.country = country
-        self.state = state
-        self.city = city
-        self.street = street
-        self.postal_code = postal
