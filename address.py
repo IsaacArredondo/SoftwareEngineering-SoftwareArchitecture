6a27f81d456edaa45a377d4b0ddc84845561fc2b
Isaac Arredondo Padrón <isaacarredondopadron@gmail.com>
2023-02-19 14:04:01 -0600

    basic structure

---
+++ address.py
@@ -0,0 +1,7 @@
+class Address:
+    def __init__(self, country, state, city, street, postal):
+        self.country = country
+        self.state = state
+        self.city = city
+        self.street = street
+        self.postal_code = postal
